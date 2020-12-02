f457f6ad3a57f3dbd5d26825397e4c5ea15aab2f
Ivo Junior <maciel.jr95@gmail.com>
2020-12-02 18:22:51 -0300

    task projetos consumida

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,58 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 import Carding from '../styles/Carding.module.css'
 import Carousel from 'nuka-carousel'
+
+import api from '../services/api';
 
 import {Header, Card,} from '../src/components'
 
 
 export default function Home() {
-  const [projects, setProjects ] = useState([
-    {
-      projeto: 'Sampling trident otima',
-      dataInit:'26/10',
-      dataFin:'02/11',
-      enderecos:[{
-        end: 'Av. Paulista, 800'
-      }]
-    },
-    {
-      projeto: 'Mesa Interativa',
-      dataInit:'26/10',
-      dataFin:'02/11',
-      enderecos:[
-        {
-        end: 'Av. Paulista, 900',},
-        {
-          end: 'Rua Fulano de Tal, 1100'
-        }
-      ]
-    },
-    {
-      projeto: 'Mesa Interativa',
-      dataInit:'26/10',
-      dataFin:'02/11',
-      enderecos:[{
-        end: 'Av. Ibirapuera, 900'
-      }]
-    },
-  ])
+  const [projects, setProjects] = useState({})
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+      // console.log((response.data));
+
+    })
+
+  }, [])
+
+  // const [projects, setProjects ] = useState([
+  //   {
+  //     projeto: 'Sampling trident otima',
+  //     dataInit:'26/10',
+  //     dataFin:'02/11',
+  //     enderecos:[
+  //       {
+  //         end: 'Av. Paulista, 800'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     projeto: 'Mesa Interativa',
+  //     dataInit:'26/10',
+  //     dataFin:'02/11',
+  //     enderecos:[
+  //       {
+  //         end: 'Av. Paulista, 900',
+  //       },
+  //       {
+  //         end: 'Rua Fulano de Tal, 1100'
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     projeto: 'Mesa Interativa',
+  //     dataInit:'26/10',
+  //     dataFin:'02/11',
+  //     enderecos:[{
+  //       end: 'Av. Ibirapuera, 900'
+  //     }]
+  //   },
+  // ]);
 
   return (
     <div className={styles.container}>
@@ -66,29 +82,32 @@ export default function Home() {
 
               <Carousel
                 defaultControlsConfig={{
-                  nextButtonClassName: Carding.buttonCarousel, 
-                  prevButtonClassName: Carding.buttonCarousel, 
+                  nextButtonClassName: Carding.buttonCarousel,
+                  prevButtonClassName: Carding.buttonCarousel,
                   pagingDotsContainerClassName: Carding.dottes
                 }}
-                wrapAround={true}  
+                wrapAround={true}
                 style={{ padding: 20, height: '89%' }}>
 
-                  {projects.map((project, i) =>(
+                  {
+
+                  Object.keys(projects).length != 0 ? projects.map((project, i) =>(
                     <div className={Carding.projectTitle} key={i}>
-                      <p className={Carding.endereco}>{project.projeto}</p>
-                      <p className={Carding.endereco}>Veiculação: {project.dataInit} - {project.dataFin}</p>
+                      <p className={Carding.endereco}>{project.name}</p>
+                      <p className={Carding.endereco}>Veiculação: {project.initial_date} - {project.final_date}</p>
                       <p className={Carding.endereco} style={{display:'flex'}}>
                         Endereço: 
-                        {project.enderecos.map((endereco, i) => (
+                        {project.address}
+                        {/* {project.enderecos.map((endereco, i) => (
                           <div style={{paddingRight:10}} key={i}>
                             {endereco.end},
                           </div>
-                        ))}
+                        ))} */}
                       </p>
 
                       <a>Ler Mais</a>
                     </div>
-                  ))}
+                  )): <di><p>Não existe projetos no momento</p></di>}
               </Carousel>
             </Card>
           </div>
